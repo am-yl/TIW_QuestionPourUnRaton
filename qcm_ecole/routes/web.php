9e2252a3b0077dcfa15fab0b91b90f204e84c154
norapcn <nora_pinchon@hotmail.fr>
2022-05-23 11:28:37 +0200

    commit tout

---
+++ qcm_ecole/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionnaireController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::prefix('/questionnaires')->middleware(['auth'])->controller(QuestionnaireController::class)->name('questionnaires.')->group(function () {
+
+    Route::get('', 'index')->name('index');
+
+    Route::get('/new', 'create')->name('new');
+
+    Route::post('/store', 'store')->name('store');
+
+    Route::get('/{id}', 'show')->name('show');
+
+    Route::get('{id}/edit/', 'edit')->name('edit');
+
+    Route::put('{id}/update', 'update')->name('update');
+
+    Route::get('/{id}/delete', 'destroy')->name('delete');
+
+});
+
+require __DIR__.'/auth.php';
