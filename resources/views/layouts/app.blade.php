0253a63cfac387dce32af5b5dfd780e900e5acc4
norapcn <nora_pinchon@hotmail.fr>
2022-06-06 16:24:01 +0200

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link href="{{ mix('public/css/style.css') }}" rel="stylesheet">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="public/css/style.css">
+        <link href="{{ mix('public/css/style.css') }}" rel="stylesheet">
 
         <title>Accueil</title>
 
@@ -73,7 +73,7 @@
 <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="max-w-md w-full space-y-8">
     <div>
-      <img class="mx-auto h-12 w-auto" src="public/img/logo.png" alt="logo">
+      <img class="mx-auto h-12 w-auto" src="{{ mix('public/img/logo.png') }}" alt="logo">
       <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">QUESTIONS POUR UN RATON</h2>
     </div>
     <form class="mt-8 space-y-6" action="#" method="POST">
@@ -99,8 +99,6 @@
       <div>
         <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            <!-- Heroicon name: solid/lock-closed -->
-            </svg>
           </span>
           CONNEXION
         </button>
