c11e9825434f329bb14334728ec2d74271ef6b54
am-yl <amyl@caramail.com>
2022-07-12 23:47:44 +0200

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-	<div class="min-h-full flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-		<div class="max-w-md w-full space-y-8">
+	<div class="h-screen flex flex-col items-center justify-center m-auto sm:px-6 lg:px-8">
+		<div class="max-w-md">
 			<img class="logo" src="{{asset('/img/raccoon.png')}}" alt="logo">
 			<h1 class="mt-6 text-center text-3xl font-extrabold">QUESTIONS POUR UN RATON</h1>
 		</div>
@@ -10,7 +10,7 @@
 				@auth
 				<a class="btnWelcome" href="{{ url('/dashboard') }}">Dashboard</a>
 				@else
-				<a class="btnWelcome mr-2" href="{{ route('login') }}">Se connecter</a>
+				<a class="btnWelcome mr-1" href="{{ route('login') }}">Se connecter</a>
 
 				@if (Route::has('register'))
 				<a class="btnWelcome" href="{{ route('register') }}">S'enregistrer</a>
