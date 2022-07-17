15945a7af7f62849d82e5eb6a68442590a47880c
am-yl <amyl@caramail.com>
2022-07-17 21:45:38 +0200

    ligne en trop

--- resources/views/groupes.blade.php
+++ resources/views/groupes.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-
     <div class="gestion my-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h2 class="titre">Gérer vos groupes</h2>
         <a class="btnNav inline-block mt-6 mb-8" href="{{ route('groupes.new') }}">Ajouter un groupe</a>
